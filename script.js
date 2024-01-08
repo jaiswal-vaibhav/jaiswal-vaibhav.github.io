d0e96bdfbdf462dfc9eae7f3229e0e7880cdf39f
Vaibhav Jaiswal <46678215+jaiswal-vaibhav@users.noreply.github.com>
2024-01-08 14:25:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log("Website loaded!");
 });
 document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+});
+
 });
